5f703c99617e7c52f1f27f0b3323175af5404d91
ZeroX2624 <Ernest.Evan.Ng@gmail.com>
2016-11-13 15:28:12 -0500

    Fire Edits

---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class RestartGame : MonoBehaviour 
+{
+
+	public void OnStartButton()
+	{
+		SceneManager.LoadScene(0);
+	}
+
+
+}
